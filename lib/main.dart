b301201280dd0bbc995d869eba2d443c7214512c
roadblock8574 <sagarbatwal1@gmail.com>
2025-04-28 16:04:21 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MainApp extends StatelessWidget
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
+      debugShowCheckedModeBanner: true,
       title: "MiniZomato",
       theme: ThemeData(
         primaryColor: Colors.blueAccent,
